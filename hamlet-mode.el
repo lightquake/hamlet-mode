d3dfef497584736d58a00a532de7bfe4923140e0
Patrick Hurst <phurst@amateurtopologist.com>
2012-03-07 23:01:09 -0500

    highlight tag names

---
+++ hamlet-mode.el
@@ -0,0 +1,16 @@
+; An emacs mode for editing files written in Hamlet, Yesod's HTML-like templating language.
+
+(defvar hamlet-mode-hook nil)
+
+; autoload on .hamlet files
+(add-to-list 'auto-mode-alist '("\\.hamlet\\'" . hamlet-mode))
+
+
+(defconst hamlet-font-lock-keywords
+  '(("<\\(\\w+\\)" 1 font-lock-function-name-face)))
+
+
+
+(define-derived-mode hamlet-mode fundamental-mode "Hamlet"
+  "Major mode for editing Hamlet files."
+  (setq font-lock-defaults '(hamlet-font-lock-keywords)))
