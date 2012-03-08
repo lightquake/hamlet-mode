8a4db0f6b90d26383fc853e803476e8671955ab2
Patrick Hurst <phurst@amateurtopologist.com>
2012-03-07 23:10:15 -0500

    highlight .class and #id attributes

--- hamlet-mode.el
+++ hamlet-mode.el
@@ -6,11 +6,14 @@
 (add-to-list 'auto-mode-alist '("\\.hamlet\\'" . hamlet-mode))
 
 
-(defconst hamlet-font-lock-keywords
-  '(("<\\(\\w+\\)" 1 font-lock-function-name-face)))
-
+(defconst hamlet-font-lock-highlighting
+  '(
+    ("<\\(\\w+\\)" 1 font-lock-function-name-face)
+    ("[.#]\\(\\(\\w\\|-\\)+\\)" . font-lock-variable-name-face)
+    )
+  )
 
 
 (define-derived-mode hamlet-mode fundamental-mode "Hamlet"
   "Major mode for editing Hamlet files."
-  (setq font-lock-defaults '(hamlet-font-lock-keywords)))
+  (setq font-lock-defaults '(hamlet-font-lock-highlighting)))
