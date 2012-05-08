89878f4753267ad7bb8d6a216300ae1fdc6d74bc
Patrick Hurst <phurst@amateurtopologist.com>
2012-05-08 19:13:59 -0400

    2-width tabstops

--- hamlet-mode.el
+++ hamlet-mode.el
@@ -1,6 +1,11 @@
 ; An emacs mode for editing files written in Hamlet, Yesod's HTML-like templating language.
 (defvar hamlet-mode-hook nil)
 
+; tab stops from column 2 to 80. more than that, and you're doing it wrong.
+(add-hook 'hamlet-mode-hook 'lambda ()
+          (setq tab-stop-list (number-sequence 2 80 2)))
+
+
 ; autoload on .hamlet and .lucius files
 (add-to-list 'auto-mode-alist '("\\.hamlet\\'" . hamlet-mode))
 (add-to-list 'auto-mode-alist '("\\.lucius\\'" . css-mode))
