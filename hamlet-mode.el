c4ef3f1f00dc5c2a06fe8957433a958017261e4b
Kata <lightquake@amateurtopologist.com>
2013-12-06 15:18:57 -0500

    Bit of cleanup in prep for a rewrite.

--- hamlet-mode.el
+++ hamlet-mode.el
@@ -1,14 +1,43 @@
-; An emacs mode for editing files written in Hamlet, Yesod's HTML-like templating language.
-(defvar hamlet-mode-hook nil)
+;;; hamlet-mode.el --- Hamlet editing mode
+
+;; Author: Kata <lightquake@amateurtopologist.com
+;; Keywords: wp, languages, comm
+
+;; Copyright (c) 2012 Kata
+
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+
+;; The above copyright notice and this permission notice shall be included in
+;; all copies or substantial portions of the Software.
+
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+
+(defgroup hamlet nil
+  "Hamlet editing mode."
+  :group 'languages)
+
+(defcustom hamlet-mode-hook nil
+  "Hook run by hamlet-mode."
+  :type 'hook
+  :group 'hamlet)
+
+;; Associate ourselves with hamlet files.
+(add-to-list 'auto-mode-alist '("\\.hamlet\\'" . hamlet-mode))
 
 ; tab stops from column 2 to 80. more than that, and you're doing it wrong.
 (add-hook 'hamlet-mode-hook '(lambda ()
           (setq tab-stop-list (number-sequence 2 80 2))))
-
-
-; autoload on .hamlet and .lucius files
-(add-to-list 'auto-mode-alist '("\\.hamlet\\'" . hamlet-mode))
-(add-to-list 'auto-mode-alist '("\\.lucius\\'" . css-mode))
 
 (defconst hamlet-name-regexp "[_:[:alpha:]][-_.:[:alnum:]]*")
 
